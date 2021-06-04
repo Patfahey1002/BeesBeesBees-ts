b3a941ac31e0dad6c03e5c17dfea7f2a497db049
Mircetic <17263@nelnet.net>
2021-06-04 09:49:32 -0600

    all done

--- src/ForagerBee.ts
+++ src/ForagerBee.ts
@@ -1,2 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Bee {
+    canFly: boolean;
+    treasureChest: string[];
+    forage(): void {
+        this.treasureChest.push('treasure');
+    }
+
+    constructor(age: number, color: string, food: string, eat: string, job: string) {
+        super(age, color, food, eat);
+        this.age = 10;
+        this.job = 'find pollen';
+        this.canFly = true;
+        this.treasureChest = [];
+    }
 };
--- src/RetiredForager.ts
+++ src/RetiredForager.ts
@@ -1,2 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
+    gamble(): void {
+        this.treasureChest.push('treasure');
+    }
+    
+    
+    constructor(age: number, color: string, food: string, eat: string, job: string) {
+        super(age, color, food, eat, job);
+        this.age = 40;
+        this.job = 'gamble';
+        this.canFly = false;
+        this.color = 'grey';
+        }
+        
+        forage(): string {
+            return 'I am too old, let me play cards instead';
+    }
 };
